69c73b204237c50d8f429c6d1287f8bedcf52901
zhi <linkxsystem@gmail.com>
2021-03-02 19:40:00 +0800

    feat: updated the main.go , writed the test function

--- src/main.go
+++ src/main.go
@@ -5,6 +5,8 @@ import "io/ioutil"
 import "os"
 import "binchunk"
 
+import . "vm"
+
 func main() {
 	if len(os.Args) > 1 {
 		data, err := ioutil.ReadFile(os.Args[1])
@@ -53,7 +55,49 @@ func printCode(f *binchunk.Prototype) {
 		if len(f.LineInfo) > 0 {
 			line = fmt.Sprintf("%d", f.LineInfo[pc])
 		}
-		fmt.Printf("\t%d\t[%s]\t0x%08X\n", pc+1, line, c)
+
+		i := Instruction(c);
+
+		// fmt.Printf("\t%d\t[%s]\t0x%08X\n", pc+1, line, i.OpName())
+		fmt.Printf("\t%d\t[%s]\t%s \n", pc+1, line, i.OpName())
+		printOperands(i)
+		fmt.Printf("\n")
+	}
+}
+
+func printOperands(i Instruction) {
+	switch i.OpMode() {
+	case IABC:
+		a, b, c := i.ABC()
+		fmt.Printf("%d", a)
+		if i.BMode() != OpArgN {
+			if b > 0xFF {
+				fmt.Printf(" %d", -1 - b & 0xFF);
+			} else {
+				fmt.Printf(" %d", b)
+			}
+		}
+		if i.CMode() != OpArgN {
+			if c > 0xFF {
+				fmt.Printf(" %d", -1 - c & 0xFF)
+			} else {
+				fmt.Printf(" %d", c)
+			}
+		}
+	case IABx: 
+		a, bx := i.ABx()
+		fmt.Printf("%d", a)
+		if i.BMode() == OpArgK {
+			fmt.Printf(" %d", -1 - bx)
+		} else {
+			fmt.Printf(" %d", bx)
+		}
+	case IAsBx:
+		a, sbx := i.AsBx()
+		fmt.Printf("%d %d", a, sbx)
+	case IAx: 
+		ax := i.Ax()
+		fmt.Printf("%d", -1 - ax)
 	}
 }
 
